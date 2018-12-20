b422e9c6fb1227480f426ac3209c4fc26ff1f9b0
WUQUANLONG <2424349030@qq.com>
2018-12-20 20:36:21 +0800

    add spacemacs script

--- install-emacs-on-centos.sh
+++ install-emacs-on-centos.sh
@@ -15,3 +15,21 @@ cd emacs-25.2
 ./autogen.sh
 ./configure –without-makeinfo –with-x-toolkit=no –with-xpm=no –with-jpeg=no –with-png=no –with-gif=no –with-tiff=no
 make install
+
+if [ -e ~/.emacs.d ]
+then
+	mv ~/.emacs.d ~/.emacs.d.bak
+ 	git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+else
+ 	git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+fi
+
+if [ -e ~/.spacemacs.d ]
+then
+	mv ~/.spacemacs.d ~/.spacemacs.d.bak
+	git clone https://github.com/waterloo3122/my_spacemacs_private.git ~/.spacemacs.d
+else
+	git clone https://github.com/waterloo3122/my_spacemacs_private.git ~/.spacemacs.d
+fi
+
+
