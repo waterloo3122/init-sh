dc90f21781fca1a46506a8516a10a25b6cf6bcbe
pp <panyouyi@licaimofang.com>
2018-12-21 18:22:12 +0800

    add fix

--- config-centos-after-install.sh
+++ config-centos-after-install.sh
@@ -3,8 +3,9 @@
 setenforce 0
 sed -i "s/enforcing/disabled/g" `grep enforcing -rl /etc/selinux/config`
 
-systemctl disable firewalld
-systemctl stop firewalld
-systemctl status firewalld
+# do this when needed
+#systemctl disable firewalld
+#systemctl stop firewalld
+#systemctl status firewalld
 
 
