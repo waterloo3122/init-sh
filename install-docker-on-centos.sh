d33030fff63e28403a2255e1c0b89708bc23f722
Peter Pan <panyouyi@gmail.com>
2019-02-15 15:45:05 +0800

    Update install-docker-on-centos.sh

--- install-docker-on-centos.sh
+++ install-docker-on-centos.sh
@@ -9,3 +9,10 @@ systemctl enable docker
 curl -sSL https://get.daocloud.io/daotools/set_mirror.sh | sh -s http://f1361db2.m.daocloud.io
 systemctl start docker
 systemctl status docker
+
+# on centos 6
+rpm -iUvh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+yum update -y
+yum -y install docker-io
+service docker start
+chkconfig docker on
