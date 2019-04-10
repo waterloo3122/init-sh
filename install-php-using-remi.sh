4af1ae382239b07e5ac6aae9ce7141d218a77584
Peter Pan <panyouyi@gmail.com>
2019-04-10 15:37:10 +0800

    Create install-php-using-remi.sh

---
+++ install-php-using-remi.sh
@@ -0,0 +1,13 @@
+# refer to 
+https://www.tecmint.com/install-php-7-in-centos-7/
+```
+yum install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
+yum install yum-utils
+yum-config-manager --enable remi-php56
+yum install php php-mcrypt php-cli php-gd php-curl php-mysql php-ldap php-zip php-fileinfo php-fpm composer
+```
+
+#set composer chinese mirror
+```
+ composer config -g repo.packagist composer https://packagist.laravel-china.org
+```
