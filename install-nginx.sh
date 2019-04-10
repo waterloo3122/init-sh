15ad3888a36ca1ce7ed9b760e9bb7e01f62ae46a
Peter Pan <panyouyi@gmail.com>
2019-04-10 15:46:26 +0800

    Create install-nginx.sh

---
+++ install-nginx.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cat << EOF >> /etc/yum.repos.d/nginx.repo 
+> [nginx]
+> name=nginx repo
+> baseurl=http://nginx.org/packages/centos/\$releasever/\$basearch/
+> gpgcheck=0
+> enabled=1
+> EOF
+
