462f5db7ef34a4d80d390994372aa3cef7e001bb
pp <panyouyi@licaimofang.com>
2019-05-05 17:30:12 +0800

    add fix

--- install-docker-on-centos.sh
+++ install-docker-on-centos.sh
@@ -11,8 +11,8 @@ systemctl start docker
 systemctl status docker
 
 # on centos 6
-rpm -iUvh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
-yum update -y
-yum -y install docker-io
-service docker start
-chkconfig docker on
+#rpm -iUvh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+#yum update -y
+#yum -y install docker-io
+#service docker start
+#chkconfig docker on
